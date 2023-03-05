a23ecb7029673737076ec34a58081fd663c4d0f1
gumingWu <15626185054@163.com>
2023-03-05 23:46:23 +0800

    use title

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: 'Blogs'
+})
+</script>
 
 <template>
   im blog
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: 'Projects'
+})
+</script>
 
 <template>
   im projects
