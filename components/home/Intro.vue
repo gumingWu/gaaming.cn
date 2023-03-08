df78237836f9b02f260d545e888af62feef549bd
gumingWu <15626185054@163.com>
2023-03-08 10:34:35 +0800

    change intro animation

--- components/home/Intro.vue
+++ components/home/Intro.vue
@@ -1,15 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const hiRef = ref()
+const imRef = ref()
+const fendRef = ref()
+
+useMotion(hiRef, {
+  initial: {
+    x: -100,
+    opacity: 0
+  },
+  enter: {
+    x: 0,
+    opacity: 1,
+  }
+})
+
+useMotion(imRef, {
+  initial: {
+    x: -100,
+    opacity: 0
+  },
+  enter: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 100,
+    },
+  }
+})
+
+useMotion(fendRef, {
+  initial: {
+    x: -100,
+    opacity: 0
+  },
+  enter: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 200,
+    },
+  }
+})
+</script>
 
 <template>
   <div >
-      <div flex text="3xl md:4xl xl:5xl" >
+      <div ref="hiRef" flex text="3xl md:4xl xl:5xl" >
         Hi!
         <div i-twemoji:man-superhero-light-skin-tone dark:i-twemoji:ninja-light-skin-tone ></div>
       </div>
-      <div text="5xl md:6xl xl:7xl" font="700" >
+      <div ref="imRef" text="5xl md:6xl xl:7xl" font="700" >
         I'm GaamingWu
       </div>
-      <div text="2xl md:3xl xl:4xl" >
+      <div ref="fendRef" text="2xl md:3xl xl:4xl" >
         <div>
         A front-end developer
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-const { introRef, imageRef } = useHomeMotion()
+const { imageRef } = useHomeMotion()
 </script>
 
 <template>
   <div max-w-7xl mt-30 mx-auto md:flex="~ row-reverse" >
     <HomeImage ref="imageRef" flex-1 />
-    <HomeIntro ref="introRef" flex-1 />
+    <HomeIntro flex-1 />
   </div>
 </template>
