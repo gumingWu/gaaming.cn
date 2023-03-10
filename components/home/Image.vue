8ce2bc9a56f99f33294347063b8231a52d087b44
gumingWu <15626185054@163.com>
2023-03-10 17:59:21 +0800

    fix mobile home image change theme error

--- components/home/Image.vue
+++ components/home/Image.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div i-twemoji:panda dark:i-twemoji:bear transition-all duration-500 mx-auto text="10rem" md:text="20rem" ></div>
+  <div i-twemoji:panda dark:i-twemoji:bear transition="none sm:all" duration-500 mx-auto text="10rem" md:text="20rem" ></div>
 </template>
 
 <style scoped></style>
