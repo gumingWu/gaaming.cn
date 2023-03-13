d787f55c91a3c6c48b992d4c99c01c4515a47d1f
gumingWu <15626185054@163.com>
2023-03-14 00:34:05 +0800

    project card

---
+++ components/project/Card.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <div class="project-card" w-80 h-100 bg="#f5f5f5 dark:slate-800/25" backdrop-grayscale p="x-8 y-6" rounded-md cursor-pointer >
+    {{ title }}
+  </div>
+</template>
+
+<style scoped>
+.project-card {
+  transition: box-shadow .3s ease, transform .2s ease;
+}
+.project-card:hover {
+  box-shadow: 0 8px 50px #23232333;
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,10 +2,24 @@
 useHead({
   title: 'Projects'
 })
+
+const projects = [
+  {
+    title: 'wiew-ui'
+  },
+  {
+    title: 'gaaming.cn'
+  },
+  {
+    title: 'vitepress-fun'
+  }
+]
 </script>
 
 <template>
-  im projects
+  <div max-w-6xl mx-auto mt-10 flex="~ wrap" gap-10 >
+    <ProjectCard v-for="pj in projects" :key="pj.title" :title="pj.title" />
+  </div>
 </template>
 
 <style scoped></style>
